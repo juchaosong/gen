6735086ebc3b043fabb4b5c06e106df3b01765ef
Song Juchao <juchao@coscene.io>
2023-11-23 21:50:32 +0800

    feat: support all types

--- condition.go
+++ condition.go
@@ -26,12 +26,7 @@ func (c *condContainer) CondError() error    { return c.err }
 func exprToCondition(exprs ...clause.Expression) []Condition {
 	conds := make([]Condition, 0, len(exprs))
 	for _, e := range exprs {
-		switch e := e.(type) {
-		case *datatypes.JSONQueryExpression, *datatypes.JSONOverlapsExpression, *datatypes.JSONArrayExpression:
-			conds = append(conds, &condContainer{value: e})
-		default:
-			conds = append(conds, &condContainer{err: fmt.Errorf("unsupported Expression %T to converted to Condition", e)})
-		}
+		conds = append(conds, &condContainer{value: e})
 	}
 	return conds
 }
